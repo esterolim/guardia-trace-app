e61d4a331d3829160bfaecc39d9accb56198e4d2
Bruna Ester <brunaerrolim@gmail.com>
2025-12-26 18:38:09 -0300

    feat: update AndroidManifest and add AttachmentUiModel with support for various attachment types

--- app/src/main/java/com/example/guardiantrace/presentation/model/AttachmentUiModel.kt
+++ app/src/main/java/com/example/guardiantrace/presentation/model/AttachmentUiModel.kt
@@ -1,2 +1,17 @@
 package com.example.guardiantrace.presentation.model
 
+data class AttachmentUiModel(
+    val id: String = "",
+    val incidentId: String = "",
+    val fileName: String = "",
+    val fileSize: Long = 0,
+    val mimeType: String = "",
+    val createdAt: java.time.LocalDateTime = java.time.LocalDateTime.now(),
+    val filePath: String = "",
+    val isEncrypted: Boolean = true,
+    val attachmentType: AttachmentType = AttachmentType.IMAGE
+)
+
+enum class AttachmentType {
+    IMAGE, VIDEO, AUDIO, DOCUMENT
+}
